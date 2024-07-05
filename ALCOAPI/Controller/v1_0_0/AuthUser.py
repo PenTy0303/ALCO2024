c5b9e432f9a8cd6f932dc49d4e0999d75fa04f2d
PenTy0303 <167627346+PenTy0303@users.noreply.github.com>
2024-07-05 15:23:27 +0900

    routerの整理とversion管理
    ルーターを整理してurlにverを追加しました．

--- ALCOAPI/Controller/v1_0_0/AuthUser.py
+++ ALCOAPI/Controller/v1_0_0/AuthUser.py
@@ -3,8 +3,8 @@ from flask import Blueprint
 
 # 機能分割用のBluePrint登録
 # UrlPrefixは仕様書に基づく
-AuthUser = Blueprint("AuthUser", __name__, url_prefix = "/AuthUser")
+AuthUser = Blueprint("AuthUser", __name__, url_prefix = "/v1.0.0/AuthUser")
 
 @AuthUser.route("/")
 def index():
-    return "AuthUserIndex"
+    return "AuthUser"
--- ALCOAPI/router.py
+++ ALCOAPI/router.py
@@ -2,10 +2,9 @@
 from flask import Flask
 
 # 自作モジュール（相対実行ファイルであるServerから見た相対インポートであることに注意）
-from ALCOAPI.API.AuthUser import AuthUser
-
+from ALCOAPI.Controller.v1_0_0.AuthUser import AuthUser
 app = Flask(__name__)
 app.config.from_object('ALCOAPI.config')
 
 # BluePrintへ登録
-app.register_blueprint(AuthUser) # ユーザー認証機能
+app.register_blueprint(AuthUser) # ver1のAPI群の追加
